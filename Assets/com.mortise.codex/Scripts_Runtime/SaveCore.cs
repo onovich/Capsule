64811887917816e110516cbdf76f88de011a593a
onovich <hjn1110@163.com>
2024-05-06 10:20:43 +0800

    <feature> fix: 读写异常

--- Assets/com.mortise.codex/Scripts_Runtime/SaveCore.cs
+++ Assets/com.mortise.codex/Scripts_Runtime/SaveCore.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using MortiseFrame.LitIO;
+using UnityEngine;
 
 namespace MortiseFrame.Capsule {
 
@@ -28,18 +29,10 @@ namespace MortiseFrame.Capsule {
                 FileHelper.LoadBytes(path, buff);
             }
 
-            var count = buff.Length;
             var offset = 0;
-            while (offset < count) {
-                var len = ByteReader.Read<int>(buff, ref offset);
-                if (len == 0) {
-                    break;
-                }
-                save = ctx.GetSave(key) as ISave;
-                save.FromBytes(buff, ref offset);
-            }
+            save = ctx.GetSave(key) as ISave;
+            save.FromBytes(buff, ref offset);
 
-            CLog.Log($"Load from {path}, key: {key}");
             return save;
         }
 
@@ -47,23 +40,14 @@ namespace MortiseFrame.Capsule {
             byte[] buff = ctx.WriteBuffer_Get();
             ctx.WriteBuffer_Clear();
 
-            int offset = 4;
+            int offset = 0;
             byte key = ctx.GetKey(save.GetType());
             save.WriteTo(buff, ref offset);
-
-            int len = offset;
-            offset = 0;
-            ByteWriter.Write<int>(buff, len, ref offset);
-
-            if (len == 0) {
-                return key;
-            }
 
             var saveName = ctx.GetSaveFileName(key);
             var path = Path.Combine(ctx.Path, saveName);
             FileHelper.SaveBytes(path, buff, offset);
 
-            CLog.Log($"Save to {path}, key: {key}");
             return key;
         }
 
